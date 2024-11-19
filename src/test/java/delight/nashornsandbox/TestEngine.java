aa37532329bf501e7e8f4266679b5c6c2ae947f8
bin.hu <bin.hu@ringcentral.com>
2024-11-19 15:52:32 +0800

    add clean inject variable failed test

--- src/test/java/delight/nashornsandbox/TestEngine.java
+++ src/test/java/delight/nashornsandbox/TestEngine.java
@@ -1,11 +1,15 @@
 package delight.nashornsandbox;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 import org.junit.Test;
 
 import delight.nashornsandbox.exceptions.ScriptCPUAbuseException;
 import junit.framework.Assert;
+
+import static org.junit.Assert.assertThrows;
 
 public class TestEngine {
 	
@@ -31,6 +35,44 @@ public class TestEngine {
 
 		Assert.assertEquals(null, sandbox.eval("delete this.engine; this.engine.factory;"));
 
+	}
+
+	@Test
+    public void testEngineCleanInjectVar() throws ScriptException {
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+
+		// Step 2: Write JavaScript code and execute in NashornSandbox
+		String script = " myVar = {\"aa\":\"bb\"};\n" +
+				"                workflow.log(\"Hello from NashornSandbox\");\n";
+		engine.put("myVar", "{\"Name\":{\"a\":\"b\"}}");
+		engine.put("workflow", new TestMethodJavascriptInterface());
+		//        engine.eval(script, bindings);
+		engine.eval(script);
+
+		// Step 3: Get JavaScript variable value from Nashornengine bindings
+		System.out.println("Value of myVar: " + engine.get("myVar"));
+
+		engine.put("myVar", null);
+		engine.put("workflow", null);
+		//        engine.getBindings(ScriptContext.ENGINE_SCOPE).clear();
+		assertThrows(ScriptException.class, () -> engine.eval("workflow.log(myVar);"));
+	}
+	@Test
+    public void testSandboxCleanInjectVar() throws ScriptException {
+		NashornSandbox sandbox = NashornSandboxes.create();
+		String script = "           workflow.log(myVar);\n" +
+				"                myVar = {\"aa\":\"bb\"};\n" +
+				"                workflow.log(\"Hello from NashornSandbox\");\n";
+		sandbox.inject("myVar", "{\"Name\":{\"a\":\"b\"}}");
+		sandbox.inject("workflow", new TestMethodJavascriptInterface());
+		sandbox.eval(script);
+
+		System.out.println("Value of myVar: " + sandbox.get("myVar"));
+
+		sandbox.inject("myVar", null);
+		sandbox.inject("workflow", null);
+		assertThrows(ScriptException.class, () -> sandbox.eval("workflow.log(myVar);"));
+		//        assertEquals("TypeError: null has no such function \"log\" in <eval> at line number 1", scriptException.getMessage());
 	}
 
 	
---
+++ src/test/java/delight/nashornsandbox/TestMethodJavascriptInterface.java
@@ -0,0 +1,12 @@
+package delight.nashornsandbox;
+
+
+public class TestMethodJavascriptInterface {
+
+
+    public void log(String info) {
+        String msg = String.format("TestMethodJavascriptInterface Output >> %s", info);
+        System.out.println(msg);
+    }
+
+}
